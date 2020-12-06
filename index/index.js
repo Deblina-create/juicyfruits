3524ddbd1322c0a5aaeb642f823db4aa0665b702
Deblina-create <deblina4@gmail.com>
2020-12-06 23:39:16 +0100

    initial layout of index page

---
+++ index/index.js
@@ -0,0 +1,22 @@
+class Fruit{
+    constructor(price, name, index, id, description){
+        this.prie = price;
+        
+        this.name = name;
+        this.description = description;
+    }
+}
+// make all objects in local storage on first load (write a function with if else to make sure it loads only once )
+//make an array of the objects for the local storage to access
+
+//access the array in local storage using ...getItem
+
+//function for loading fruits from local storage
+
+
+
+
+$(document).ready(function(){
+
+});
+
